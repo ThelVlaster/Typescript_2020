f7752a9d7d306dc5374f83c16c7a3c122393d0bf
Ryan Shawn Manansala <rmanansala@BLASTASIA.local>
2020-04-03 19:34:57 +0800

    deleted manansala

--- Manansala/Exercise2.js
+++
@@ -1,5 +0,0 @@
-var input = 4;
-var GetPerimeter = function (input) { return (input * 4); };
-var GetArea = function (input) { return (input * input); };
-console.log("Perimeter = " + GetPerimeter(input));
-console.log("Area = " + GetArea(input));
--- Manansala/Exercise3.js
+++
@@ -1,9 +0,0 @@
-var input = "Hi";
-var StringConcat = function (input, number) {
-    var endString = "";
-    for (var i = 0; i < number; i++) {
-        endString += input;
-    }
-    return endString;
-};
-console.log(StringConcat(input, 4));
